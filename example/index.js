2f1a4b6f6bec2a3ad573df011cfb45eb5f03b386
Ionică Bizău <bizauionica@gmail.com>
2016-08-19 16:11:52 +0300

    Using HackCSS by @egoist, on the demo page. :sparkles:

--- example/index.js
+++ example/index.js
@@ -1,9 +1,7 @@
 "use strict";
 domRepeater([
-    { value: 0, text: "Mate-info" }
-  , { value: 1, text: "Foo <strong>Bold</strong>" }
-  , { value: 1, text: "Foo <script>alert()</script>" }
-  , { value: 1, text: "Foo" }
-  , { value: 1, text: "Foo" }
-  , { value: 1, text: "Foo" }
+    { value: 0, text: "apple" }
+  , { value: 1, text: "pear" }
+  , { value: 2, text: "orange" }
+  , { value: 3, text: "lemon" }
 ]);
